c7a731ae2294cd19856a76eeef6f2520041e59a7
Strekalovskyi D <ortin@i.ua>
2017-03-07 09:41:05 +0200

    add test

--- src/main/java/com/nitro/Avtor.java
+++ src/main/java/com/nitro/Avtor.java
@@ -1,6 +1,7 @@
 package com.nitro;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -10,7 +11,7 @@ public class Avtor {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @OneToMany(mappedBy = "avtor")
+    @OneToMany(mappedBy = "avtor", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     private List<Book> books;
 
     private String name;
@@ -37,6 +38,7 @@ public class Avtor {
     }
 
     public void setBooks(List<Book> books) {
+
         this.books = books;
     }
 
@@ -55,4 +57,14 @@ public class Avtor {
     public void setSecondName(String secondName) {
         this.secondName = secondName;
     }
+
+    public String toString(){
+        return "avtor [ "+getName()+" "+getSecondName()+" book: "+getBooks();
+    }
+    public void addBook(Book book){
+        if (books == null) books=new ArrayList<Book>();
+        books.add(book);
+
+    }
+
 }
--- src/test/java/com/nitro/OnetomanyApplicationTests.java
+++ src/test/java/com/nitro/OnetomanyApplicationTests.java
@@ -3,14 +3,19 @@ package com.nitro;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OnetomanyApplicationTests {
+	private static final Logger logger = LoggerFactory.getLogger(OnetomanyApplicationTests.class);
 
 
 	@Autowired
@@ -25,6 +30,12 @@ public class OnetomanyApplicationTests {
 		Avtor first = new Avtor("Semen", "Arsenyev");
 		Avtor second = new Avtor("Alex", "Jersy");
 
+		first.addBook(new Book("My life"));
+		second.addBook(new Book("Second of wins"));
+		first.addBook(new Book("My firs of dance"));
+		second.addBook(new Book("Seconds of death"));
+
+		avtorRepo.save(Arrays.asList(first,second));
 	}
 
 
@@ -34,7 +45,23 @@ public class OnetomanyApplicationTests {
 
 
 	@Test
-	public void contextLoads() {
+	public void countAvtor() {
+		logger.info("*****************");
+		logger.info("**** counts: "+avtorRepo.count());
+		logger.info("*****************");
 	}
+	@Test
+	public  void countBook(){
+		logger.info("****************");
+		logger.info("**** count books: "+bookRepo.count());
+		logger.info("*****************");
+	}
+	@Test
+	public void getAllAvtor(){
+		logger.info("*****************");
+		logger.info(avtorRepo.findAll().toString());
+	}
+
+
 
 }
